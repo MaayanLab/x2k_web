1f2b00a9a0967f737003bb32725d274b733253d4
Maxim Kuleshov <kuleshov.max.v@gmail.com>
2017-10-05 16:56:47 -0400

    It's actually working

--- src/main/webapp/bootstrap-frontend/js/index.js
+++ src/main/webapp/bootstrap-frontend/js/index.js
@@ -9,11 +9,16 @@ function submitButtonListener(button, endpoint, settings_form) {
     $('#' + button).click(function (evt) {
         // evt.preventDefault();
         var $form = $(settings_form),
-            text_input;
+            text_input = $("#genelist").val();
 
         $form.attr("action", endpoint);
-        text_input = $("#genelist").val();
-        console.log(JSON.stringify($form.serializeArray()))
+        
+        // Hack that works like a charm        
+        var input = $("<input>")
+        	.attr("type", "hidden")
+        	.attr("name", "text-genes").val(text_input);
+        $form.append($(input));
+        
         if (text_input.length > 0) {
             $form.submit();
         }
@@ -21,9 +26,17 @@ function submitButtonListener(button, endpoint, settings_form) {
 }
 
 $(function () {
-    submitButtonListener("chea_submit", "ChEA", "#chea-form");
-    submitButtonListener("kea_submit", "KEA", "#kea-form");
-    // submitButtonListener("x2k_submit", "network");
-    submitButtonListener("g2n_submit", "G2N", "#g2n-form");
-    submitButtonListener("results_submit", "results", "#x2k-form");
+	$(".desc").hide();
+	$("#x2k-desc").show();
+    submitButtonListener("chea_submit", "http://localhost:8080/X2K/ChEA", "#chea-form");
+    submitButtonListener("kea_submit", "http://localhost:8080/X2K/KEA", "#kea-form");
+    // submitButtonListener("x2k_submit", "http://localhost:8080/X2K/network");
+    submitButtonListener("g2n_submit", "http://localhost:8080/X2K/G2N", "#g2n-form");
+    submitButtonListener("results_submit", "http://localhost:8080/X2K/results", "#x2k-form");
+    
+    $("#nav-x2k-tab").on("click", function(){$(".desc").hide(); $("#x2k-desc").show();})
+    $("#nav-chea-tab").on("click", function(){$(".desc").hide(); $("#chea-desc").show();})
+    $("#nav-g2n-tab").on("click", function(){$(".desc").hide(); $("#g2n-desc").show();})
+    $("#nav-kea-tab").on("click", function(){$(".desc").hide(); $("#kea-desc").show();})
+    
 });
