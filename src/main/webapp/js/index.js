cc8063ce762259605690c5ac72e815e53c1b3e9e
Maxim Kuleshov <kuleshov.max.v@gmail.com>
2017-11-08 17:25:44 -0500

    Pre-deployment

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,201 +1,71 @@
-/* index.js: Primary JavaScript file for handling user interface.
- */
-
-/* Checks for page loading with hash or hash changing.
- */
-function listenForHashChange() {
-    if (hasHash()) {
-        highlightTabAndShowContent();
-    } else {
-        window.location.hash = "x2k";
-    }
-    $(window).on('hashchange', highlightTabAndShowContent);
-}
-
-/* Highlights the selected/active tab and shows the appropriate content.
- */
-function highlightTabAndShowContent() {
-    var hash = getHash(),
-        $tabToHighlight = $('#menu a[href="#' + hash + '"]'),
-        $tabContentToShow = $('#' + hash),
-        $inputFields = $('#input-fields'),
-        $runButtons = $('#run-buttons').parent();
-    clearTabContent();
-    $tabToHighlight.addClass('active');
-    $tabContentToShow.fadeIn(600);
-    if ($tabToHighlight.hasClass('information')) {
-        $inputFields.hide();
-        $runButtons.hide();
-    } else {
-        $inputFields.show();
-        $runButtons.show();
-    }
-}
-
-function clearTabContent() {
-    $('.tab-content').hide();
-    $('.tab-link').removeClass('active');
-}
-
-/* Returns true if URL contains characters beyond hash symbol, False otherse.
- */
-function hasHash() {
-    var h = window.location.hash;
-    return !!h && h !== '#';
-}
-
-/* Returns URL hash, stripping the "#" symbol.
- */
-function getHash() {
-    return window.location.hash.substring(1);
-}
-
-/* Listens for the user to click on the "Example gene list" button.
- */
-function listenForExample() {
-    $('#example-gene-list').click(insertExample);
-}
-
-/* Fetches and inserts example genes.
- */
 function insertExample() {
-    $.get('resources/example_genes', function(data) {
-        $('textarea#text-genes').val(data);
+    $.get('static/example_list.txt', function (data) {
+        $('textarea#genelist').val(data);
     });
     return false;
 }
 
-//var GRAPH_WIDTH = 300;
-//var GRAPH_HEIGHT = 290;
-//
-//function histogram(data, labels, title, canvas, width, height) {
-//    var ctx = document.getElementById(canvas).getContext("2d");
-//    ctx.canvas.height = height;
-//    ctx.canvas.width = width;
-//    var myChart = new Chart(ctx, {
-//        type: 'line',
-//        data: {
-//            labels: labels.slice(1,labels.length),
-//            datasets: [
-//                {
-//                    lineTension: .1,
-//                    pointHoverBackgroundColor: "rgba(0, 152, 255, 1)",
-//                    pointHoverBorderColor: "rgba(0, 152, 255, 1)",
-//                    backgroundColor: "rgba(154, 214, 255, 1)",
-//                    borderColor: "rgba(0, 152, 255, 1)",
-//                    label: title,
-//                    data: data
-//                }
-//            ]
-//        },
-//        options: {
-//            maintainAspectRatio: true,
-//            responsive: false,
-//            legend: {
-//                display: false},
-//            scales: {
-//                yAxes: [{
-//                    scaleLabel: {
-//                        display: true,
-//                        labelString: 'Nodes'
-//                    }
-//                }],
-//                xAxes: [{
-//                    scaleLabel:{
-//                        display: true,
-//                        labelString: "Interactions per Node "
-//                    }
-//                }]
-//            }
-//        }
-//    });
-//}
-//
-//function statsDiv(dataset_name, data) {
-//    dataset = data[dataset_name];
-//    div_string = '<div id=' + dataset_name + ' class="stats">' +
-//        '<h4 class="stats_name">' + dataset_name.replace(/_/g, ' ') + '</h4>' +
-//        'Total Interactions: ' + dataset.count +
-//        '<canvas id=' + dataset_name + '_graph></canvas>' +
-//        '</div>';
-//    return div_string;
-//}
-//
-////data type refers to KEA, ChEA, or G2N
-//function buildHistograms(data, data_type, datasets){
-//    for(i = 0; i < datasets.length; i++){
-//        dataset_name = datasets[i];
-//        $( "#" + data_type + "_stats" ).append(statsDiv(dataset_name,data[data_type]));
-//        histogram(data[data_type][dataset_name].data,
-//            data[data_type][dataset_name].labels,
-//            "Number of interactors",
-//            dataset_name + "_graph",
-//            GRAPH_WIDTH,
-//            GRAPH_HEIGHT);
-//    }
-//}
-
-function submitButtonListener(button, endpoint){
-    $('#' + button).click(function(evt) {
-        evt.preventDefault();
-        var $form = $("#settings_form"),
-            text_input,
-            file_input;
+function submitButtonListener(button, endpoint, settings_form) {
+    $('#' + button).click(function (evt) {
+        // evt.preventDefault();
+        var $form = $(settings_form),
+            text_input = $("#genelist").val();
 
         $form.attr("action", endpoint);
-        text_input = $("#text-genes").val();
-        file_input = $("#file_upload").val();
-        if (text_input.length > 0 || file_input.length > 0){
+
+        // Hack that works like a charm
+        var input = $("<input>")
+            .attr("type", "hidden")
+            .attr("name", "text-genes").val(text_input);
+        $form.append($(input));
+
+        if (text_input.length > 0) {
             $form.submit();
-            // TODO: Implement a better loader.
         }
     });
 }
 
-$(function() {
+function showToolDesc(tool) {
+    $("#nav-" + tool + "-tab").on("click", function () {
+        $(".desc").hide();
+        $("#" + tool + "-desc").show();
+        $("#analysis-row").show();
+    })
+}
 
-    listenForHashChange();
-    listenForExample();
-
-    // Get the thing that submits to different servlets
-    submitButtonListener("chea_submit", "ChEA");
-    submitButtonListener("kea_submit", "KEA");
-    submitButtonListener("x2k_submit", "network");
-    submitButtonListener("g2n_submit", "G2N");
-    submitButtonListener("results_submit", "results");
-
-    // Accordion
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-
-    for (i = 0; i < acc.length; i++) {
-        acc[i].onclick = function(e){
-        	e.preventDefault();
-            this.classList.toggle("active");
-            this.nextElementSibling.classList.toggle("show");
+function showHelpDesc(page) {
+    $("#nav-" + page + "-tab").on("click", function () {
+        $(".desc").hide();
+        $("#analysis-row").hide();
+        
+        if (page === "datasets") {
+        	
         }
-    }
-    
-    
-//    // Display stats
-//    $.ajax({
-//        url: 'datasets/dataset_statistics.json',
-//        success: function(data) {
-//            buildHistograms(
-//                data,
-//                "ChEA",
-//                ["ChEA_2015", "ENCODE_2015", "ChEA_&_ENCODE_Consensus", "Transfac_&_Jaspar"]
-//            );
-//            buildHistograms(
-//                data,
-//                "G2N",
-//                ["Biocarta", "BioGrid", "DIP", "innateDB", "IntAct", "KEGG", "MINT", "ppid", "SNAVI"]
-//            );
-//            buildHistograms(
-//                data,
-//                "KEA",
-//                ["Kinase-Protein", "Phosphorylation"]
-//            );
-//        }
-//    })   
+    });
+}
+
+$(function () {
+    submitButtonListener("results_submit", "http://localhost:8080/X2K/results", "#x2k-form");
+    submitButtonListener("results_submit_ljp", "http://localhost:8080/X2K/results", "#x2k-ljp-form");
+    // submitButtonListener("x2k_submit", "http://localhost:8080/X2K/network");
+    submitButtonListener("chea_submit", "http://localhost:8080/X2K/ChEA", "#chea-form");
+    submitButtonListener("kea_submit", "http://localhost:8080/X2K/KEA", "#kea-form");
+    submitButtonListener("g2n_submit", "http://localhost:8080/X2K/G2N", "#g2n-form");
+
+
+    showToolDesc("x2k");
+    showToolDesc("chea");
+    showToolDesc("g2n");
+    showToolDesc("kea");
+
+    showHelpDesc("about");
+    showHelpDesc("api");
+    showHelpDesc("commandline");
+    showHelpDesc("datasets");
+
+    $("#nav-case-tab").on("click", function () {
+        $(".desc").hide();
+        $("#case-desc").show();
+        $("#analysis-row").hide();
+    })
 });
